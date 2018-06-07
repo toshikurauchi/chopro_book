7e394f00103f68ef0bb3fa8f70858003a93007cb
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2018-06-07 10:53:30 -0300

    Styling transpose

--- static/js/chopro.js
+++ static/js/chopro.js
@@ -1,1 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, {"coverTrigger": false, "alignment": "right"});
+});
+
 M.AutoInit();
